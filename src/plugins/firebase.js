bd8447fc941a4be463aa5c0a217e427710dd108a
codewiz <c.chaiwat.ch@gmail.com>
2021-10-12 22:55:38 +0700

    update: add firebase before deploy

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -2,15 +2,14 @@ import firebase from "firebase/app";
 import 'firebase/auth'
 
 
-
-  var firebaseConfig = {
-    apiKey: "AIzaSyApPCSLHs8nZySR267zmFLxzOGEDPLK7C0",
-    authDomain: "vue2d-vtf-82c3d.firebaseapp.com",
-    projectId: "vue2d-vtf-82c3d",
-    storageBucket: "vue2d-vtf-82c3d.appspot.com",
-    messagingSenderId: "537954887481",
-    appId: "1:537954887481:web:9a72f767f7fe8093adaae8"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyCNq3gaGHzli6ERcevXRarMbrLk5qPy9dQ",
+  authDomain: "vue-app-yai.firebaseapp.com",
+  projectId: "vue-app-yai",
+  storageBucket: "vue-app-yai.appspot.com",
+  messagingSenderId: "573590879835",
+  appId: "1:573590879835:web:87afde607df815e0e5497e"
+};
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
